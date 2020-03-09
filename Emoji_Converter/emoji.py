74a54defbb3988d34325a7f3301ed14174492df6
Zeerik45 <f178037@nu.edu.pk>
2020-03-09 09:53:46 +0500

    only converts 2 Emojis

---
+++ Emoji_Converter/emoji.py
@@ -0,0 +1,25 @@
+message = input('>')
+message1=message.split(" ")
+#split the message and save as a list
+emojis={
+    ':)':'😊',
+    ':(':'😥',
+}
+output=''
+for i in message1:
+    output += emojis.get(i,i)+" "
+print(output)
+#############################################
+def emj(message):
+        message1 = message.split(" ")
+        # split the message and save as a list
+        emojis = {
+            ':)': '😊',
+            ':(': '😥',
+        }
+        output = ''
+        for i in message1:
+            output += emojis.get(i, i) + " "
+        print(output)
+message = input('>')
+emj(message)
